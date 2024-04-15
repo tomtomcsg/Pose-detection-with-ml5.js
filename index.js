2271924c2b9212528cd1334b9971dabea93017b1
Tommy Suarez <159101789+tomtomcsg@users.noreply.github.com>
2024-04-15 08:21:57 -0400

    added the skeleton to be drawn on the webcam

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function draw() {
     image(video, 0, 0);
 
     drawDots();
+    drawSkeleton();
 }
 
 function drawDots() {
@@ -38,6 +39,19 @@ function drawDots() {
                 noStroke();
                 ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
             }
+        }
+    }
+}
+
+function drawSkeleton() {
+    for (let i = 0; i < poses.length; i++) {
+        const skeleton = poses[i].skeleton;
+
+        for (let j = 0; j < skeleton.length; j++) {
+            let partA = skeleton[j][0];
+            let partB = skeleton[j][1];
+            stroke(255, 0, 0);
+            line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
         }
     }
 }
