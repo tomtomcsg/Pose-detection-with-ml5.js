98c745a744364d11bb933444452c587140b22457
Tommy Suarez <159101789+tomtomcsg@users.noreply.github.com>
2024-04-14 23:55:51 -0400

    created a function to track the keypoints on the webcam

--- index.js
+++ index.js
@@ -12,8 +12,32 @@ function setup() {
         poses = results;
         //console.log(poses);
     });
+    video.hide();
 }
 
 function videoLoaded() {
     console.log("VIDEO WORKING");
+}
+
+function draw() {
+    image(video, 0, 0);
+
+    drawDots();
+}
+
+function drawDots() {
+    // loops through all the poses detected
+    for (let i = 0; i < poses.length; i += 1) {
+        const pose = poses[i].pose;
+
+        for (let j = 0; j < pose.keypoints.length; j += 1) {
+            const keypoint = pose.keypoints[j];
+
+            if (keypoint.score > 0.2) {
+                fill(255, 0, 0);
+                noStroke();
+                ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+            }
+        }
+    }
 }
